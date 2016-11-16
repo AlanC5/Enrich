8dafcbee193a9d840d8ee20de26d1ff341161039
Anthony Shalagin <anthony.shalagin@gmail.com>
2016-11-16 16:47:31 -0500

    Fixed CharField max_length attribute

--- Enrich/organization_staff_models.py
+++ Enrich/organization_staff_models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class Organization_Staff(models.Model):
     organization_staff_id = models.IntegerField(primary_key = True)
-    email = models.CharField()
-    password = models.CharField()
-    name = models.CharField()
-    title = models.CharField()
+    email = models.CharField(max_length = 100)
+    password = models.CharField(max_length = 30)
+    name = models.CharField(max_length = 100)
+    title = models.CharField(max_length = 100)
     organization_id = models.IntegerField() #Make foreign key later
--- user/models.py
+++ user/models.py
@@ -2,8 +2,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 class User(models.Model):
-    user_id = models.CharField(primary_key = True)
-    email = models.CharField()
-    password = models.CharField()
-    name = models.CharField()
-    school_name = models.CharField()
+    user_id = models.CharField(primary_key = True, max_length = 100)
+    email = models.CharField(max_length = 100)
+    password = models.CharField(max_length = 30)
+    name = models.CharField(max_length = 100)
+    school_name = models.CharField(max_length = 100)
