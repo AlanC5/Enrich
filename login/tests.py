c9c614d6ab253ad0a1183762102e90a6bb087b21
slamwell17 <scohen000@citymail.cuny.edu>
2016-11-17 14:50:17 -0500

    Added a unit test to check that the login page exists.

--- login/tests.py
+++ login/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
-
+from django.test.client import RequestFactory
+from .views import *
 # Create your tests here.
+
+class LoginTestCase(TestCase):
+    def setUp(self):
+        self.rf = RequestFactory()
+
+    def test_login_page_exists(self):
+        """Makes sure the login page index returns a 200 OK"""
+
+        get_request = self.rf.get("/login/")
+        response = index(get_request)
+        self.assertEqual(response.status_code, 200)
+
+
