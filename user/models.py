7b857eaf9e2561fd3eabe314557cfa8cb058d34c
Alan Chen <alanc1995@gmail.com>
2016-11-25 15:31:20 -0500

    Adjusted primary key fields to be autoincrement in models

--- user/models.py
+++ user/models.py
@@ -2,11 +2,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 class User(models.Model):
-    user_id = models.CharField(primary_key = True, max_length = 100)
+    user_id = models.AutoField(primary_key = True)
     email = models.CharField(max_length = 100)
     password = models.CharField(max_length = 30)
     name = models.CharField(max_length = 100)
     school_name = models.CharField(max_length = 100)
 
     def __str__(self):
-        return self.user_id + ', ' + self.email + ', ' + self.name + ', ' + self.school_name
+        return str(self.user_id) + ', ' + self.email + ', ' + self.name + ', ' + self.school_name
