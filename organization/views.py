cf7965ad989b7e2d3f1aa1be535d0d1a7443bde5
KhadeejaDin <khadeejadin@gmail.com>
2016-11-28 12:15:48 -0500

    Render all organizations on organization page

--- organization/views.py
+++ organization/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from models import Organization
 
 def index(request):
-    return render(request, 'organization/home.html')
+    organizations = Organization.objects.all()
+    return render(request, 'organization/home.html', {'organizations': organizations})
 
 def organization_page(request, o_name):
     organization = Organization.objects.filter(name=o_name)
