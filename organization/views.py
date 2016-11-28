3dfb9b3af72b6f3524f9e6be0c9cb455d4285ceb
KhadeejaDin <khadeejadin@gmail.com>
2016-11-28 11:28:28 -0500

    Basic request to organization page

--- organization/views.py
+++ organization/views.py
@@ -3,3 +3,7 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'organization/home.html')
+
+def organization_page(request, o_name):
+    organization = Organization.objects.filter(name=o_name)
+    return render(request, 'organization/', {'organization': organization})
