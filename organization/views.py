57a02fee7aca33a01146e9a4530c16260b16e98c
KhadeejaDin <khadeejadin@gmail.com>
2016-11-29 11:46:57 -0500

    Add geopy to create geocode address into latitude and longitude

--- organization/views.py
+++ organization/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from models import Organization
+from geopy.geocoders import Nominatim
+
+geolocator = Nominatim()
 
 def index(request):
     organizations = Organization.objects.all()
     return render(request, 'organization/home.html', {'organizations': organizations})
 
-def organization_page(request, o_name):
-    organization = Organization.objects.filter(name=o_name)
-    return render(request, 'organization/', {'organization': organization})
+def organization_page(request, name):
+    organization = Organization.objects.filter(name = name)
+    address = organization[0].address
+    print address
+    location = geolocator.geocode(address)
+    latitude = location.latitude
+    longitude = location.longitude
+    print latitude, longitude
+    return render(request, 'organization/organization.html', {'organization': organization[0], 'latitude': latitude, 'longitude': longitude})
