cb37c657e9081e41497ffabee80a11556c47b576
slamwell17 <scohen000@citymail.cuny.edu>
2016-12-01 10:18:40 -0500

    Began working on pylint coverage

--- organization/tests.py
+++ organization/tests.py
@@ -1,5 +1,6 @@
-from django.test import TestCase
+"""Tests the organization app"""
 
+from django.test import TestCase
 from .models import Organization
 
 # Create your tests here.
@@ -7,40 +8,40 @@ from .models import Organization
 def OrganizationTestCase(TestCase):
 
     def setUp(self):
-        self.rf = RequestFactory()
-        Organization.objects.create( organization_id = 1,
-            name = "a",
-            category = "a",
-            description = "blah",
-            free = False,
-            tuition = 1000,
-            rating = 2,
-            address = "123 Elm St",
-            contact_number = "1111",
-            website = "www.enrich.edu",
-            imageURL = "a")
+        """Sets up test db"""
+        Organization.objects.create(organization_id=1,
+                                    name="a",
+                                    category="a",
+                                    description="blah",
+                                    free=False,
+                                    tuition=1000,
+                                    rating=2,
+                                    address="123 Elm St",
+                                    contact_number="1111",
+                                    website="www.enrich.edu",
+                                    imageURL="a")
 
     def test_entering_the_db(self):
-
-        entry = Organization.objects.get(organization_id = 1)
+        """Tests if I entered into the database"""
+        entry = Organization.objects.get(organization_id=1)
 
         self.assertTrue(entry.name == "a")
 
     def test_adding_and_removing_from_db(self):
+        """Tests if I can add and remove to/from db"""
 
-        Organization.objects.create(
-            name = "b",
-            category = "a",
-            description = "blah",
-            free = False,
-            tuition = 1000,
-            rating = 2,
-            address = "123 Elm St",
-            contact_number = "1111",
-            website = "www.enrich.edu",
-            imageURL = "a")
+        Organization.objects.create(name="b",
+                                    category="a",
+                                    description="blah",
+                                    free=False,
+                                    tuition=1000,
+                                    rating=2,
+                                    address="123 Elm St",
+                                    contact_number="1111",
+                                    website="www.enrich.edu",
+                                    imageURL="a")
 
-        entry = Organization.objects.get(organization_id = 2)
+        entry = Organization.objects.get(organization_id=2)
         self.assertNotNull(entry)
 
         Organization.objects.remove(organization_id=2)
