bf51c198c550478487d5ee869793541db14fd4a2
Alan Chen <alanc1995@gmail.com>
2016-12-03 01:04:26 -0500

    Finish styling homepage buttons

--- search/static/js/search.js
+++ search/static/js/search.js
@@ -1,3 +1,28 @@
 $(document).ready(function() {
-    console.log(5);
+    $("#STEM_button").on("click", function() {
+        $(this).toggleClass("btn-primary");
+        $("#id_category_0").prop("checked", !$("#id_category_0").prop("checked"));
+    });
+
+    $("#arts_button").on("click", function() {
+        $(this).toggleClass("btn-primary");
+        $("#id_category_1").prop("checked", !$("#id_category_1").prop("checked"));
+    });
+
+    $("#academic_button").on("click", function() {
+        $(this).toggleClass("btn-primary");
+        $("#id_category_2").prop("checked", !$("#id_category_2").prop("checked"));
+    });
+
+    $("#sports_button").on("click", function() {
+        $(this).toggleClass("btn-primary");
+        $("#id_category_3").prop("checked", !$("#id_category_3").prop("checked"));
+    });
+
+    $("#tuition_button").on("click", function() {
+        $(this).toggleClass("btn-primary");
+        $("#id_price_0").prop("checked", !$("#id_price_0").prop("checked"));
+    });
+
+
 });
