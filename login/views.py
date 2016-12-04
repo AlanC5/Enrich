d1f2f0060bd30344d92c0e7979e39f5499af0a79
KhadeejaDin <khadeejadin@gmail.com>
2016-12-04 13:00:55 -0500

    import authenticate and login

--- login/views.py
+++ login/views.py
@@ -1,6 +1,5 @@
-"""Views for the login"""
-
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 #from django.http import HttpResponse
 
 # Create your views here.
