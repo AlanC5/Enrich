ba5e7f58e03287bcc2037967d1694a04bf8ed448
Samuel Cohen <scohen000@citymail.cuny.edu>
2016-12-04 21:35:32 -0500

    More Enrich module tests

--- Enrich/tests.py
+++ Enrich/tests.py
@@ -9,9 +9,12 @@ class EnrichTestCase(TestCase):
         organizations = Organization.objects
         reviews = Reviews.objects
 
-    def test_login_page_exists(self):
+    def test_organization_model(self):
         """Makes sure the login page index returns a 200 OK"""
 
         get_request = self.reqs.get("/login/")
         response = index(get_request)
         self.assertEqual(response.status_code, 200)
+
+    def test_review_model(self):
+        self.assertTrue(False)
