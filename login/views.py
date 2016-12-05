ae5732e44c2a118a2ed2dc6982889b01b56dbb5f
KhadeejaDin <khadeejadin@gmail.com>
2016-12-05 09:35:00 -0500

    Add login, logout, register methods

--- login/views.py
+++ login/views.py
@@ -1,45 +1,48 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
+#from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 from .forms import RegistrationForm
 from django.views.generic import View
-from user.models import User
-#from django.http import HttpResponse
+from user.models import EnrichUser
+from django.contrib.auth.models import User
+from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
-    """Main login page"""
-    return render(request, "login/login_register.html")
 
-class RegistrationFormView(View):
-    form_class = RegistrationForm
-    template_name = 'registration/login_register.html'
+def logout_user(request):
+    logout(request)
+    return render(request, 'search/search.html')
 
-    #display blank form
-    def get(self, request):
-        form = self.form_class(None)
-        return render(request, self.template_name, {'form': form})
 
-    def post(self, request):
-        form = self.form_class(request.POST)
+def login_user(request):
+    print("here")
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        print (user)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                curr_user = EnrichUser.objects.filter(user=request.user)
+                print(curr_user)
+                return render(request, 'search/search.html', {'user': curr_user})
+        else:
+            return render(request, 'login/login_register.html', {'error_message': 'Invalid login'})
+    return render(request, 'login/login_register.html')
 
-        if form.is_valid():
-            user = form.save(commit = False)
 
-            name = form.cleaned_data['name']
-            password = form.cleaned_data['password']
-            user.set_password(password)
-            #user.save()
-            User.objects.create(user)
-
-            user = authenticate(email=email, password=password)
-
-            if user is not None:
-
-                if user.is_active:
-
-                    login(request, user)
-                    return redirect('search:index')
-
-        return render(request, self.template_name, {'form': form})
-
-            #user_id=User.objects.get(pk=user_id),
+def register(request):
+    form = RegistrationForm(request.POST or None)
+    if form.is_valid():
+        user = User.objects.create_user(
+            username=form.cleaned_data['username'],
+            email=form.cleaned_data['email'],
+            password=form.cleaned_data['password'],
+        )
+        return render(request, 'search/search.html')
+    context = {
+        "form": form,
+    }
+    return render(request, 'login/login_register.html', context)
