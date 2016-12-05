e10600973c0f305521b7ea4f753bc3e9e186569e
KhadeejaDin <khadeejadin@gmail.com>
2016-12-05 09:54:21 -0500

    Update migrations after merge

--- Enrich/migrations/0002_auto_20161124_2100.py
+++ Enrich/migrations/0002_auto_20161124_2100.py
@@ -26,6 +26,6 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='reviews',
             name='user_id',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='user.User'),
+            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='user.EnrichUser'),
         ),
     ]
