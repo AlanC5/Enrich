302afa17924fd9712bf8284f50a9d2d11d5a5972
KhadeejaDin <khadeejadin@gmail.com>
2016-12-05 14:11:58 -0500

    New user created

--- login/urls.py
+++ login/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     url(r'^$', views.login_user, name='login_user'),
     url(r'^register/$', views.register, name='register'),
     url(r'^login_user/$', views.login_user, name='login_user'),
-    url(r'^logout_user/$', views.logout_user, name='logout_user'),
-    url(r'^profile/$', views.update_profile, name='update_profile')]
+    url(r'^logout_user/$', views.logout_user, name='logout_user')]
